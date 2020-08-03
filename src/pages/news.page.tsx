326e143d31c749d3114865f156b800f896e4d3d7
Jacky Wang <www.wangzixiao@gmail.com>
2020-08-04 00:16:18 +0800

    content update

--- src/pages/news.page.tsx
+++ src/pages/news.page.tsx
@@ -27,20 +27,45 @@ export class NewsPage extends Component {
                                         <div className="media-content">
                                             <div className="content">
                                                 <p className="title has-text-black is-4">
+                                                    SARYA is proud to be certified by CME Group
+                                                </p>
+                                                <p className="subtitle is-6">July 22, 2020</p>
+                                                <hr />
+                                                <p>
+                                                    SARYA FINSOFT is proud to announce that the firm is certified by 
+                                                    CME Group for both iLink 2.x market access and MDP 3.0 market data.
+                                                </p>
+                                                <p>
+                                                    The certificate enables us to deliver very low-latency and stable 
+                                                    CME market data and trading opportunities to our clients in the 
+                                                    region. Especially, our current clients trading precious metals are 
+                                                    now immediately powered to COMEX with great trading functionality, 
+                                                    speed and stability.
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </article>
+                                </div>
+
+                                <div className="box">
+                                    <article className="media">
+                                        <div className="media-content">
+                                            <div className="content">
+                                                <p className="title has-text-black is-4">
                                                     Partnership with Ausiris Co., Ltd for building Algo Strategy Trading component for TFEX
                                                 </p>
                                                 <p className="subtitle is-6">September, 2019</p>
-                                                <hr/>
+                                                <hr />
                                                 <p>
-                                                    Sarya has partnered with Ausiris Co., Ltd. (Ausiris), 
-                                                    Thailand’s top gold bullion house to build Algo Strategy 
-                                                    Trading component for TFEX (Thailand Futures Exchange). 
+                                                    Sarya has partnered with Ausiris Co., Ltd. (Ausiris),
+                                                    Thailand’s top gold bullion house to build Algo Strategy
+                                                    Trading component for TFEX (Thailand Futures Exchange).
                                                 </p>
                                                 <p>
-                                                    Sarya will leverage YAS Pro Trader Programme Trading Workstations 
-                                                    for Ausiris to build tailor-made prop trading tools for 
-                                                    active market making empowered by data analytics. Future 
-                                                    projects also include building Next-generation Trading Platform 
+                                                    Sarya will leverage YAS Pro Trader Programme Trading Workstations
+                                                    for Ausiris to build tailor-made prop trading tools for
+                                                    active market making empowered by data analytics. Future
+                                                    projects also include building Next-generation Trading Platform
                                                     using Heterogeneous Computing and Machine Learning.
                                                 </p>
                                             </div>
@@ -56,49 +81,25 @@ export class NewsPage extends Component {
                                                     Partnership with Classic Ausiris for the Next-generation Professional Trading Platform.
                                                 </p>
                                                 <p className="subtitle is-6">July 1st, 2019</p>
-                                                <hr/>
+                                                <hr />
                                                 <p>
-                                                    Sarya is happy to announce a partnership with Classic 
-                                                    Ausiris Investment Advisory Securities Co.,Ltd. (“CAF”) 
+                                                    Sarya is happy to announce a partnership with Classic
+                                                    Ausiris Investment Advisory Securities Co.,Ltd. (“CAF”)
                                                     for the Next-generation Professional Trading Platform.
                                                 </p>
                                                 <p>
-                                                    Sarya will provide Classic Ausiris, who is member of 
-                                                    Thailand Futures Exchange (TFEX), and one of the top 
-                                                    futures brokers in Thailand, tailor-made solutions 
-                                                    including YAS TFEX Connectivity, Market Data and Order 
-                                                    Sending connectivity to TFEX, and YAS Market Making 
-                                                    workstations for Futures and Options. 
+                                                    Sarya will provide Classic Ausiris, who is member of
+                                                    Thailand Futures Exchange (TFEX), and one of the top
+                                                    futures brokers in Thailand, tailor-made solutions
+                                                    including YAS TFEX Connectivity, Market Data and Order
+                                                    Sending connectivity to TFEX, and YAS Market Making
+                                                    workstations for Futures and Options.
                                                 </p>
                                                 <p>
-                                                    This partnership is another important step for Sarya 
-                                                    towards our goal of being the best technological partner 
-                                                    for professional traders with our proven superior execution 
+                                                    This partnership is another important step for Sarya
+                                                    towards our goal of being the best technological partner
+                                                    for professional traders with our proven superior execution
                                                     capability to capture market opportunity by low-latency market access.
-                                                </p>
-                                            </div>
-                                        </div>
-                                    </article>
-                                </div>
-
-                                <div className="box">
-                                    <article className="media">
-                                        <div className="media-content">
-                                            <div className="content">
-                                                <p className="title has-text-black is-4">
-                                                    Sarya has passed the complete conformance test of TFEX (Thailand Futures Exchange)
-                                                </p>
-                                                <p className="subtitle is-6">April, 2019</p>
-                                                <hr/>
-                                                <p>
-                                                    Sarya is proud to announce that we have passed the
-                                                    complete conformance test of TFEX (Thailand Futures Exchange).
-                                                </p>
-                                                <p>
-                                                    The conformance test certified us to be able to perform manual
-                                                    trading, algo trading and market making across all types of
-                                                    products listed in TFEX such as SET50 Index options,
-                                                    Gold Futures, Rubber Futures, Interest Rate Futures, etc.
                                                 </p>
                                             </div>
                                         </div>
--- src/pages/products/market-connectivity.page.tsx
+++ src/pages/products/market-connectivity.page.tsx
@@ -25,9 +25,10 @@ export class MarketConnectivityPage extends Component {
                             <div className="columns">
                                 <div className="column is-9">
                                     <h3 className="title has-text-black is-3">Market Connectivity</h3>
-                                    <p>
-                                        Native Gateways
-                                    </p>
+                                    <ul>
+                                        <li>Native Gateways</li>
+                                        <li>Certified by CME Group for both iLink 2.x market access and MDP 3.0 market data</li>
+                                    </ul>
                                 </div>
                             </div>
                             <div className="columns">
@@ -35,7 +36,7 @@ export class MarketConnectivityPage extends Component {
                                     <div className="content">
                                         <label className="label">TFEX (Thailand Futures Exchange)</label>
                                         <ul>
-                                            <li>Certified since Feb 2019</li>
+                                            <li>Certified since 1 August 2020</li>
                                             <li>Native EMAPI</li>
                                             <li>Support all Order Types</li>
                                             <li>Page by Page Quoting</li>
